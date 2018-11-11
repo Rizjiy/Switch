a998520decedba7dac9276707b1b5fabd6906bbc
Rizjiy <rodal2807@gmail.com>
2018-11-11 18:44:22 +0300

    check

--- Bme280Control/Bme280Control.ino
+++ Bme280Control/Bme280Control.ino
@@ -1,43 +1,19 @@
-/***************************************************************************
-  This is a library for the BME280 humidity, temperature & pressure sensor
-
-  Designed specifically to work with the Adafruit BME280 Breakout
-  ----> http://www.adafruit.com/products/2650
-
-  These sensors use I2C or SPI to communicate, 2 or 4 pins are required
-  to interface. The device's I2C address is either 0x76 or 0x77.
-
-  Adafruit invests time and resources providing this open source code,
-  please support Adafruit andopen-source hardware by purchasing products
-  from Adafruit!
-
-  Written by Limor Fried & Kevin Townsend for Adafruit Industries.
-  BSD license, all text above must be included in any redistribution
- ***************************************************************************/
 
 #include <Wire.h>
-#include <SPI.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
 
-#define BME_SCK 13
-#define BME_MISO 12
-#define BME_MOSI 11
-#define BME_CS 10
-
 #define BME280_ADDRESS  (0x77)
-#define SEALEVELPRESSURE_HPA (1013.25)
-
 #define BME280_ADDRESS2  (0x76)
 
 Adafruit_BME280 bme; // I2C
 Adafruit_BME280 bme2; // I2C
-//Adafruit_BME280 bme(BME_CS); // hardware SPI
-//Adafruit_BME280 bme(BME_CS, BME_MOSI, BME_MISO, BME_SCK); // software SPI
+
 
 unsigned long delayTime;
 
 void setup() {
+	\
 	Serial.begin(115200);
 
 	// default settings
