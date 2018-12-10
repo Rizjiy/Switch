5380d4b849deb39dc3ba93eed0d6befc3c9b9e5f
lukich <lukich@srv.int>
2018-12-10 21:44:11 +0300

    2

--- Beta/Test/Test.ino
+++ Beta/Test/Test.ino
@@ -13,8 +13,17 @@ using namespace std;
 class ConnectionHelper {
 public:
 	ConnectionHelper(const char* ssid, const char* wifiPass, const char* mqttServer, const int mqttPort, const char* mqttUser, const char* mqttPass, string deviceName);
-	bool MqttConnect();
+	void handle();
+
+	RBD::Timer reconnectTimer; //пауза между реконнектами Wi-Fi
+
+	WiFiClient wifiClient;
+	PubSubClient mqttClient;
+
 private:
+	bool wifiConnect();
+	bool mqttConnect();
+
 	const char* _ssid;
 	const char* _wifiPass;
 	const char* _mqttServer;
@@ -23,9 +32,9 @@ private:
 	const char* _mqttPass;
 
 	string _deviceName;
+	string _baseTopic = "home/switches";
 
-	WiFiClient _wclient;
-	PubSubClient _mqttclient;
+	string _topicSubscribe;
 
 
 };
@@ -40,34 +49,53 @@ ConnectionHelper::ConnectionHelper(const char* ssid, const char* wifiPass, const
 
 	_deviceName = deviceName;
 
-	WiFiClient _wclient;
-	PubSubClient _mqttclient(_wclient);
+	RBD::Timer reconnectTimer(60000); //пауза между реконнектами Wi-Fi
 
+	WiFiClient wifiClient;
+	PubSubClient mqttClient(wifiClient);
+
+	_topicSubscribe = _baseTopic + "/" + _deviceName + "/#";		// home/switches/switch5/#
 }
 
-bool ConnectionHelper::MqttConnect()
+bool ConnectionHelper::wifiConnect()
+{
+	if (WiFi.status() != WL_CONNECTED) {
+		Serial.print("Connecting to ");
+		Serial.print(_ssid);
+		Serial.println("...");
+		WiFi.begin(_ssid, _wifiPass);
+
+		if (WiFi.waitForConnectResult() != WL_CONNECTED)
+			return false;
+		Serial.println("WiFi connected");
+	}
+
+	return true;
+}
+
+bool ConnectionHelper::mqttConnect()
 {
 	// Loop until we're reconnected
-	if (!_mqttclient.connected())
+	if (!mqttClient.connected())
 	{
 		Serial.print("Attempting MQTT connection to ");
 		Serial.print(_mqttServer);
 		Serial.println("...");
 		// Attempt to connect
 		//if (client.connect("ESP8266Client", mqtt_user, mqtt_pass))
-		if (_mqttclient.connect(_deviceName.c_str(), _mqttUser, _mqttPass))
+		if (mqttClient.connect(_deviceName.c_str(), _mqttUser, _mqttPass))
 		{
 			Serial.println("connected");
 			// Once connected, publish an announcement...
-			string startPayload = _deviceName + ' ' + String(_wclient.localIP()).c_str();
-			_mqttclient.publish("Start", startPayload.c_str());
+			string startPayload = _deviceName + ' ' + String(wifiClient.localIP()).c_str();
+			mqttClient.publish("Start", startPayload.c_str());
 			// ... and resubscribe
-			//mqttclient.subscribe(topicSubscribe.c_str()); // подписываемся нв топики для этого устройства
+			mqttClient.subscribe(_topicSubscribe.c_str()); // подписываемся нв топики для этого устройства
 		}
 		else
 		{
 			Serial.print("failed, rc=");
-			Serial.println(_mqttclient.state());
+			Serial.println(mqttClient.state());
 			return false;
 		}
 	}
@@ -75,27 +103,51 @@ bool ConnectionHelper::MqttConnect()
 	return true;
 }
 
+void ConnectionHelper::handle() {
 
-class MqttSwitch {
+	// подключаемся к wi-fi
+	if (reconnectTimer.isExpired())
+	{
+		if (wifiConnect())
+			if (mqttConnect())
+			{
+				//Все ок
+				mqttClient.loop();
+			}
+			else
+			{
+				reconnectTimer.restart();
+				//Mqtt не подключился
+			}
+		else
+		{
+			reconnectTimer.restart();
+			//Wi-fi не подключился
+		}
+	}
+
+}
+
+
+class MqttButton {
 public:
-	MqttSwitch(byte relayPin, byte buttonPin, string string);
+	MqttButton(byte relayPin, byte buttonPin, string string);
 private:
 	byte _relayPin;
 	byte _buttonPin;
-	string _deviceName;
-	WiFiClient _wclient;
-	PubSubClient _mqttclient;
+	string deviceName;
+	WiFiClient wifiClient;
+	PubSubClient mqttClient;
 
 
 };
 
-MqttSwitch::MqttSwitch(byte relayPin, byte buttonPin, string deviceName) {
+MqttButton::MqttButton(byte relayPin, byte buttonPin, string deviceName) {
 	_relayPin = relayPin;
 	_buttonPin = buttonPin;
-	_deviceName = deviceName;
+	deviceName = deviceName;
 
 	pinMode(_relayPin, OUTPUT);
-
 
 }
 
